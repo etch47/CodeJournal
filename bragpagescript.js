fe1748aa15e46aaf076b35bbaabbba7a9597028b
John Fletcher <jbfletcher47@hotmail.com>
2016-01-05 11:04:12 -0600

    week 1 Oxygen Class

---
+++ bragpagescript.js
@@ -0,0 +1,7 @@
+"use strict"
+document.write("My name is John Fletcher. I wrote this on my external script page with the document write command");
+console.log("Dude you are 44 years old!");
+window.alert("I am a FIB from Illinois!");
+
+
+
